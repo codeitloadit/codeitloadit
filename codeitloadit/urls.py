33f861c4d8b662c009bcc7a671f6aa27122e98ff
codeitloadit <brian@codeitload.it>
2016-04-12 21:31:05 -0700

    Saving changes before abandoning

--- codeitloadit/urls.py
+++ codeitloadit/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import RedirectView
 
@@ -27,4 +29,4 @@ urlpatterns = [
     url(r'^register/*$', views.register, name='register'),
     url(r'^login/*$', views.login, name='login'),
     url(r'^logout/*$', views.logout, name='logout'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
